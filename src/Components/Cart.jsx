9e3f7e2e4378b61364f8911fd7063420e9fa104b
Shrinivas Hunnur <135942833+shrinivashunnur08@users.noreply.github.com>
2024-06-13 14:41:17 +0530

    modified

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import emptyCartImage from '../img/empty-cart.png'; // Adjust the path based on your project structure
+import emptyCartImage from '../assets/images/img/empty-cart.png'; // Adjust the path based on your project structure
 
 function Cart() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
         <div>
-    <div className='cart-items-empty text-xl font-bold mb-1'><h1>Oops the Cart is Empty... 😕</h1> </div>
+    <div className='cart-items-empty text-xl font-bold mb-1'><h1>Oops Your Cart is Empty... 😕</h1> </div>
      <h1 className="text-3xl font-bold mb-12"></h1> 
         <img style={{ width: '30%' }} className="mx-auto " src={emptyCartImage} alt="empty-cart" />
        
